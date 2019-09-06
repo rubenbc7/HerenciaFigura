e57d8e737c54241dd11392963fd6a71f11059b14
rubenbc7 <rubenborbollacastelo@hotmail.com>
2019-09-06 08:45:46 -0700

    Agregar archivos de proyecto.

---
+++ Herencia/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Herencia
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Triangulo triangulo = new Triangulo();
+            triangulo.color = "Azulita";
+            Console.WriteLine(triangulo.toString());
+            Console.Read();
+        }
+    }
+}
---
+++ Herencia/Triangulo.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Herencia
+{
+    class Triangulo : Figura
+    {
+        public double Base { get; set; }
+        public double Altura { get; set; }
+        public override double calcularArea()
+        {
+            return (Base* Altura)/2.0;
+        }
+        public Triangulo()
+        {
+            color = "Bien buena";
+            Base = 8;
+            Altura = 10;
+        }
+    }
+}
