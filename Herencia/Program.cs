4a7e64abbb398e424e6d6400ac628311efb5955a
alumno <alumno@E319>
2019-09-09 12:38:26 -0700

    Cambio 1

--- Herencia/Program.cs
+++ Herencia/Program.cs
@@ -13,6 +13,32 @@ namespace Herencia
             Triangulo triangulo = new Triangulo();
             triangulo.color = "Azulita";
             Console.WriteLine(triangulo.toString());
+            triangulo.Altura = 2.5;
+            triangulo.Base = 4;
+            double areaTriangulo = triangulo.calcularArea();
+            Console.WriteLine("Area de Triangulo: " + areaTriangulo);
+
+            Circulo circulo = new Circulo( 5.4);
+            Console.WriteLine("Area del circulo:" + circulo.calcularArea().ToString());
+
+            Figura triangulo2 = new Triangulo();
+            ((Triangulo)triangulo2).Base = 2;
+            ((Triangulo)triangulo2).Altura = 3;
+            Console.WriteLine("Area del triangulo #2: " + triangulo2.calcularArea().ToString());
+
+            List<Figura> figuras = new List<Figura>();
+
+            figuras.Add(triangulo);
+            figuras.Add(circulo);
+            figuras.Add(triangulo2);
+
+            Console.WriteLine(" ");
+
+            foreach (var figura in figuras)
+            {
+                Console.WriteLine("Area de figura: "+ figura.calcularArea().ToString());
+            }
+
             Console.Read();
         }
     }
